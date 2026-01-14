a40d8959ff0ec228e0bb9573197eca46f799a50d
deva-p-stack <devap677@gmail.com>
2026-01-14 08:55:02 +0530

    first commit;

--- src/main.js
+++ src/main.js
@@ -1,1 +1,27 @@
 import './style.css'
+
+
+let taskcard = document.querySelectorAll('.task-card'); 
+let inputsearch = document.querySelector('#inputsearch');
+
+inputsearch.addEventListener("input",function(){
+
+let inputvalue = inputsearch.value.toLowerCase();
+
+taskcard.forEach((cards) => {
+    let title = cards.dataset.cardTitle.toLowerCase();
+
+    if(title.includes(inputvalue)){
+       cards.style.display = "flex"; 
+    }
+    else{
+         cards.style.display = "none";
+    }
+
+
+
+});
+
+
+
+})
